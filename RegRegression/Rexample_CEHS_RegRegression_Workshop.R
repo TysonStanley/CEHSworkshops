9a40de2adbf81002f0ed35d5caa4f943934c27d8
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2017-04-13 17:28:07 -0600

    Data and code update

---
+++ RegRegression/Rexample_CEHS_RegRegression_Workshop.R
@@ -0,0 +1,27 @@
+## Code for CEHS Workshop R Example
+## Tyson S. Barrett
+## April 2017
+
+setwd("~/Box Sync/GitHub/CEHSworkshops/RegRegression/")
+
+## Data (data object called "df")
+load("WideFormat_TheOffice.RData")
+
+## STEP 1. Explore Data
+
+
+
+
+## STEP 2. Clean Data and Select Model
+
+## Clean the Data
+## 1. Missingness?
+## 2. Redundancies?
+## 3. 
+
+## Here the data is already cleaned
+
+## STEP 3. 
+
+
+
